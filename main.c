f42b08d1476c910e5dfb900841ee67615e65ba53
huangcpp <hjp1279@outlook.com>
2022-12-16 22:13:07 +0800

    init

--- main.c
+++ main.c
@@ -1,38 +1,51 @@
 #include <stdio.h>
 
 #define N 200
+int i, j;
+int cnt_a, cnt_b;
+int tag;
+
+void jiao(int *pc, int *pd) {
+    for (i = 0; i < cnt_a; i++, pc++) {
+        tag = 0;
+        for (j = 0; j < cnt_b; j++, pd++) {
+
+            if (*pc == *pd)
+                tag = 1;
+        }
+        pd -= cnt_b;
+
+        if (tag == 0)printf("%d ", *pc);
+    }
+
+
+}
+
 
 int main() {
     int a[N], b[N];
 
-    int i, j;
-    int cnt_a,cnt_b;
-    int tag;
 
-    for (i = 0,cnt_a=0; ; i++,cnt_a++) {
+    for (i = 0, cnt_a = 0;; i++, cnt_a++) {
         scanf("%5d", &a[i]);
-        if(a[i]==-1){
+        if (a[i] == -1) {
             break;
         }
     }
-    for (i = 0,cnt_b=0; ; i++,cnt_b++) {
+    for (i = 0, cnt_b = 0;; i++, cnt_b++) {
         scanf("%5d", &b[i]);
-        if(b[i]==-1){
+        if (b[i] == -1) {
             break;
         }
     }
-    for (i = 0;i<cnt_a; i++) {
-        tag = 0;
-        for (j = 0;j<cnt_b; j++)
-            if (a[i] == b[j])
-                tag = 1;
-        if (tag == 0)
-            printf("%d ", a[i]);
-    }
+
+
+    int *pa = a, *pb = b;
+    jiao(pa, pb);
+
+
     printf("\n");
 
 
     return 0;
 }
-
-
